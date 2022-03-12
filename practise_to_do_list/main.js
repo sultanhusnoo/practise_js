295ad39463e7e4b5d3d822f122fdd4652b0a0e1b
sultan <sultan.husnoo@gmail.com>
2022-03-12 12:18:33 +0400

    todo list tutorial done

--- practise_to_do_list/main.js
+++ practise_to_do_list/main.js
@@ -1,3 +1,28 @@
 //  learn-webdev
 //  To Do List| Js beginner project tutorial
 
+let addToDo = document.getElementById("addToDo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+
+addToDo.addEventListener("click", function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add("paragraph-styling");
+    
+    //  Adding an element to the list
+    paragraph.innerHTML = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    
+    //  Strike through on single click
+    paragraph.addEventListener("click", function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+
+    //  Remove item on double-click
+    paragraph.addEventListener("dblclick", function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+})
+
